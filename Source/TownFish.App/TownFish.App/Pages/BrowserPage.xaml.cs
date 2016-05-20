16c17eaa350e1439778ab040c2b28c31cd08d046
Josh Browning <josh.browning@inbox.com>
2016-05-20 17:25:30 +0100

    Updated: location search to look correct on iOS.

--- Source/TownFish.App/TownFish.App/Pages/BrowserPage.xaml.cs
+++ Source/TownFish.App/TownFish.App/Pages/BrowserPage.xaml.cs
@@ -45,8 +45,11 @@ namespace TownFish.App.Pages
 
 			BindingContextChanged += BrowserPage_BindingContextChanged;
 
-			if(Device.OS == TargetPlatform.iOS)
+			if (Device.OS == TargetPlatform.iOS)
+			{
 				topBar.Padding = new Thickness(10, 25, 10, 10);
+				TopSearchPanel.Padding = new Thickness(20, 25, 20, 10);
+			}
 		}
 
 		#endregion
