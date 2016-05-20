01205c092e799b6eb209123ae9221abd60763d90
Josh Browning <josh.browning@inbox.com>
2016-05-20 10:23:06 +0100

    Added: HR to locations listview Added: rounded corners to locations listview Updated: location search to search on enter press

--- Source/TownFish.App/TownFish.App/Pages/BrowserPage.xaml.cs
+++ Source/TownFish.App/TownFish.App/Pages/BrowserPage.xaml.cs
@@ -46,46 +46,7 @@ namespace TownFish.App.Pages
 			BindingContextChanged += BrowserPage_BindingContextChanged;
 
 			if(Device.OS == TargetPlatform.iOS)
-			{
 				topBar.Padding = new Thickness(10, 25, 10, 10);
-			}
-		}
-
-		void BrowserPage_BindingContextChanged(object sender, EventArgs e)
-		{
-			if (ViewModel != null)
-			{
-				// Because we use DisplayActionSheet
-				ViewModel.TopActionMoreCommand = new Command(async () =>
-				{
-					var action = await DisplayActionSheet("More Actions", "Cancel", null, 
-						ViewModel.OverflowImages.Select(x => x.value).ToArray<string>());
-
-					ViewModel.Source = ViewModel.OverflowImages.First(i => i.value == action).href + BrowserPageViewModel.cBaseUriParam;
-				});
-
-				ViewModel.CallbackRequested += (reqstSender, callback) =>
-				{
-					webView.InvokeJS("twnfsh.runCallback('" + callback.ToLower() + "')");
-				};
-
-				SearchPanelInput.TextChanged += SearchPanelInput_TextChanged;
-				LocationResultsListView.ItemTapped += LocationResultsListView_ItemTapped;
-			}
-		}
-
-		void LocationResultsListView_ItemTapped(object sender, ItemTappedEventArgs e)
-		{
-			ViewModel.SearchLocationHasResults = false;
-			ViewModel.SearchPanelVisible = false;
-			// TODO: Set users location
-			ViewModel.SetLocation((e.Item as TownfishLocationItem).CityID);
-		}
-
-		void SearchPanelInput_TextChanged(object sender, TextChangedEventArgs e)
-		{
-			if(e.NewTextValue.Length > 2)
-				ViewModel.UpdateLocationList(e.NewTextValue);
 		}
 
 		#endregion
@@ -109,6 +70,49 @@ namespace TownFish.App.Pages
 		void WebView_Navigating(object sender, EventArgs e)
 		{
 			ViewModel.IsLoading = true;
+		}
+
+		void BrowserPage_BindingContextChanged(object sender, EventArgs e)
+		{
+			if (ViewModel != null)
+			{
+				// Because we use DisplayActionSheet
+				ViewModel.TopActionMoreCommand = new Command(async () =>
+				{
+					var action = await DisplayActionSheet("More Actions", "Cancel", null,
+						ViewModel.OverflowImages.Select(x => x.value).ToArray<string>());
+
+					ViewModel.Source = ViewModel.OverflowImages.First(i => i.value == action).href + BrowserPageViewModel.cBaseUriParam;
+				});
+
+				ViewModel.CallbackRequested += (reqstSender, callback) =>
+				{
+					webView.InvokeJS("twnfsh.runCallback('" + callback.ToLower() + "')");
+				};
+
+				SearchPanelInput.TextChanged += SearchPanelInput_TextChanged;
+				SearchPanelInput.Completed += SearchPanelInput_Completed;
+				LocationResultsListView.ItemTapped += LocationResultsListView_ItemTapped;
+			}
+		}
+
+		void SearchPanelInput_Completed(object sender, EventArgs e)
+		{
+			if (SearchPanelInput.Text.Length > 2)
+				ViewModel.UpdateLocationList(SearchPanelInput.Text);
+		}
+
+		void LocationResultsListView_ItemTapped(object sender, ItemTappedEventArgs e)
+		{
+			ViewModel.SearchLocationHasResults = false;
+			ViewModel.SearchPanelVisible = false;
+			ViewModel.SetLocation((e.Item as TownfishLocationItem).CityID);
+		}
+
+		void SearchPanelInput_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			if (e.NewTextValue.Length > 2)
+				ViewModel.UpdateLocationList(e.NewTextValue);
 		}
 
 		#endregion
