ef9a7fd634cf344b3b8ced0fafc0f6f3a87a8129
Philip Hopkins <philip@apptelic.com>
2018-05-06 08:24:13 +0100

    - Updated to latest UberWebView with referrer fix.

--- Source/TownFish.App/TownFish.App/Helpers/ImageSourceWatcher.cs
+++ Source/TownFish.App/TownFish.App/Helpers/ImageSourceWatcher.cs
@@ -10,18 +10,18 @@ namespace TownFish.App.Helpers
 		#region ImageLoaded Attached Property
 
 		public static readonly BindableProperty ImageLoadedHandlerProperty =
-			//BindableProperty.CreateAttached ("ImageLoader", typeof (EventHandler<bool>),
-			//	typeof (ImageSource), default (EventHandler<bool>));
-			BindableProperty.CreateAttached<ImageSource, EventHandler<bool>> (b =>
-				GetImageLoadedHandler (b), default (EventHandler<bool>), BindingMode.OneWay);
+			BindableProperty.CreateAttached ("ImageLoaded", typeof (EventHandler<bool>),
+				typeof (ImageSource), default (EventHandler<bool>));
+			//BindableProperty.CreateAttached<ImageSource, EventHandler<bool>> (b =>
+			//	GetImageLoadedHandler (b), default (EventHandler<bool>), BindingMode.OneWay);
 
-		public static EventHandler<bool> GetImageLoadedHandler (BindableObject bo) =>
-			(EventHandler<bool>) bo.GetValue (ImageLoadedHandlerProperty);
+		public static EventHandler<bool> GetImageLoadedHandler (
+				BindableObject bo) =>
+					(EventHandler<bool>) bo.GetValue (ImageLoadedHandlerProperty);
 
-		public static void SetImageLoadedHandler (BindableObject bo, EventHandler<bool> value)
-		{
-			bo.SetValue (ImageLoadedHandlerProperty, value);
-		}
+		public static void SetImageLoadedHandler (
+				BindableObject bo, EventHandler<bool> value) =>
+					bo.SetValue (ImageLoadedHandlerProperty, value);
 
 		#endregion ImageLoaded Attached Property
 	}
